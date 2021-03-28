c89f80d7b44d44157a04482e62718cbfe5869823
Cole Freeman <cole.freeman@oit.edu>
2021-03-27 21:15:55 -0700

    Implemented DB API calls for hobbiesPage

--- UI/HobbySelect.js
+++ UI/HobbySelect.js
@@ -1,10 +1,27 @@
+let activityName = [];
+let activityValue = [];
+let idNum = 0;
+
+function HobbycallAPI(method, url, type, callback, params = 0) {
+    let xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.responseType = type;
+    xhr.onload = function() {
+        callback(xhr.response);
+    };
+
+    console.log(params);
+
+    xhr.send(JSON.stringify(params));
+}
+
 async function setupDropdown() {
     //const config = require('../data/all_hobbies.txt');
 
     // Get all data from file as string
     const prom = await FetchHobbies();
-    console.log(prom['hobbies']);
-
+    //console.log(prom['hobbies']);
     //parse string by ','
     let parsed = prom['hobbies'].split(',');
 
@@ -58,9 +75,15 @@ async function RatingClick(buttonPassed) {
     tempRange.value = '3';
 
     tempVal.type = "text";
-    tempVal.id = "textInput";
+    //tempVal.id = buttonPassed.textContent;
+    tempVal.id = idNum.toString();
+    idNum++;
     tempVal.value = "3";
     tempVal.style.width = "50px";
+
+    //Add to global vars
+    activityName.push(buttonPassed.textContent);
+    activityValue.push('3');
 
     tempRange.addEventListener("change",function () {
         updateVal(tempVal, tempRange.value);
@@ -71,9 +94,55 @@ async function RatingClick(buttonPassed) {
     document.getElementById('scoreBox').appendChild(tempVal);
     document.getElementById('scoreBox').appendChild(br);
 
+    //add to json file
+
     buttonPassed.remove();
+
 }
 
 function updateVal(box, val) {
+    // update json file use idNum to change json
     box.value = val;
+    activityValue[parseInt(box.id)] = val;
+}
+
+function submitActivities() {
+    /*let jsonObject = {
+        'activity': activityName.join(),
+        'value': activityValue.join()
+    };*/
+    if(activityName.length < 5)
+        alert("Select at least 5 activities");
+    else {
+        let interests = "";
+        let jj = 0;
+        for (jj = 0; jj < activityName.length; jj++) {
+            if (jj == activityName.length - 1)
+                interests += '"' + activityName[jj] + '": ' + activityValue[jj];
+            else
+                interests += '"' + activityName[jj] + '": ' + activityValue[jj] + ',';
+        }
+
+        interests = '"' + "interests" + '": { ' + interests + '}';
+        let email = "aaron.whistler@oit.edu" //need to set as actual email
+
+        let jsonText = '{ "' + "email" + '"' + ": " + '"' + email + '", ' + interests + '}';
+
+        //console.log(jsonText);
+        let jsonObj = JSON.parse(jsonText);
+        PassInterests(jsonObj);
+        //console.log(jsonObj);
+    }
+}
+
+function PassInterests(jsonOBJ) {
+    console.log(jsonOBJ);
+    let tempURL = "https://roadtrips-ml.herokuapp.com/interests/addInterests";
+    HobbycallAPI("POST", tempURL, "json", AdvanceInterestPage, jsonOBJ);
+}
+
+function AdvanceInterestPage()
+{
+    console.log("A P I   D O N E");
+    //window.location.href = "MapPage.html";
 }
